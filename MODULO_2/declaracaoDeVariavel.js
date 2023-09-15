bac8ffd12cd878b3bc362a35be77d0b28d508f17
gabriel cabral <gabrielbielcabral@gmail.com>
2023-09-15 17:22:06 -0300

    Upload de arquivo

---
+++ MODULO_2/declaracaoDeVariavel.js
@@ -0,0 +1,9 @@
+let variavelNova = 'Valor dessa variavel!!';
+
+let ultimaVarialvelDaAula
+
+ultimaVarialvelDaAula = 'Finalmente atribuí um valor'
+
+variavelNova = 'Novo Valor!!'
+console.log(variavelNova)
+console.log(ultimaVarialvelDaAula)
