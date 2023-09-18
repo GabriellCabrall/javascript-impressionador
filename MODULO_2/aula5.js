aecbfc37e2cd88cf6c81afa30e05c1e6cb662e43
gabriel cabral <gabrielbielcabral@gmail.com>
2023-09-18 20:39:53 -0300

    Formatando o código com o Prettier

---
+++ MODULO_2/aula5.js
@@ -0,0 +1,5 @@
+let variavelCaixinha1 = "Esse é meu primeiro texto do arquivo";
+const variavelCaixinha2 = "Esse é o segundo texto do arquivo!";
+
+console.log(variavelCaixinha1);
+console.log(variavelCaixinha2);
