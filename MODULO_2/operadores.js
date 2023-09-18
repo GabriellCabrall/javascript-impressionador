0aa75f09c52a09a06c277f3c7deaff2f52ded032
gabriel cabral <gabrielbielcabral@gmail.com>
2023-09-18 20:18:44 -0300

    Upload de arquivos

---
+++ MODULO_2/operadores.js
@@ -0,0 +1,13 @@
+const soma = 321 + 45;
+const sub = 930 - 492;
+const mult = 3 * 3;
+const divisao = 9/3;
+const potencia = 2**3;
+const resto = 29%7;
+
+let x = 27;
+
+x++;
+x--;
+
+console.log(x)
