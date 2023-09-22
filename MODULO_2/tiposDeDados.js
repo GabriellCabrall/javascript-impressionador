b38898b88e4d472469a01b65fded07e70ac00cec
gabriel cabral <gabrielbielcabral@gmail.com>
2023-09-21 21:15:42 -0300

    Upload de arquivo

---
+++ MODULO_2/tiposDeDados.js
@@ -0,0 +1,14 @@
+// tipo string
+const texto = "isso aqui é um texto";
+const variavel = 2332;
+const textoComVariavel = `Isso aqui é um texto com a variável ${variavel}`;
+
+console.log(textoComVariavel);
+
+// number
+const numero = 32;
+const numero2 = 54.2;
+const numero3 = -10;
+const total = numero + numero2 + numero3;
+
+console.log(total);
