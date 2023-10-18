0ebd50d284f3e8b2bc1027ba5503ef867616706d
gabriel cabral <gabrielbielcabral@gmail.com>
2023-10-17 22:19:21 -0300

    Finalizando o exercício do módulo 2

--- MODULO_2/exercicioModulo2.js
+++ MODULO_2/exercicioModulo2.js
@@ -7,13 +7,24 @@ console.log(`O valor da primeira variável é ${x}`);
 
 y = 10;
 
-console.log(
-  `O valor da minha segunda variável é ${y} e a soma das minhas duas variáveis é ${
-    x + y
-  }`
-);
+console.log(`O valor da minha segunda variável é ${y}`);
+console.log(`O valor da soma das minhas variáveis é ${x + y}`);
 
-console.log(y ** 2);
-console.log(y ** 2 / 3);
+y = y ** 2;
+console.log(y);
+
+y = y / 3;
+console.log(y);
 
 console.log("Resultado do exercício 2");
+
+const booleana1 = true;
+const booleana2 = false;
+
+console.log(`Operação AND: ${booleana1 && booleana2}`);
+console.log(`Operação OR: ${booleana1 || booleana2}`);
+
+const booleana3 = true;
+
+console.log(`Operação AND: ${booleana1 && booleana2 && booleana3}`);
+console.log(`Operação OR: ${booleana1 || booleana2 || booleana3}`);
