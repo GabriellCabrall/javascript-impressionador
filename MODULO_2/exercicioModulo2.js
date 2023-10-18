01c0b7662924410d71887a5bf82ed4f1b253ee62
gabriel cabral <gabrielbielcabral@gmail.com>
2023-10-17 21:56:40 -0300

    Respondendo a questão 1 do desafio

---
+++ MODULO_2/exercicioModulo2.js
@@ -0,0 +1,19 @@
+console.log("Resultado do exercício 1");
+
+const x = 2148;
+let y;
+
+console.log(`O valor da primeira variável é ${x}`);
+
+y = 10;
+
+console.log(
+  `O valor da minha segunda variável é ${y} e a soma das minhas duas variáveis é ${
+    x + y
+  }`
+);
+
+console.log(y ** 2);
+console.log(y ** 2 / 3);
+
+console.log("Resultado do exercício 2");
