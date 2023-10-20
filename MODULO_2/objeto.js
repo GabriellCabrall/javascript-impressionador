d754968127a2f7cb17745546bf2dbe216315ceea
gabriel cabral <gabrielbielcabral@gmail.com>
2023-10-19 21:49:10 -0300

    Upload de arquivo

---
+++ MODULO_2/objeto.js
@@ -0,0 +1,11 @@
+const documentoIdentidade = {
+  nome: "Gabriel",
+  sobrenome: "Cabral",
+  cpf: "111.111.111-11",
+  empresa: "Telebras",
+  idade: 22,
+};
+
+console.log(documentoIdentidade.nome);
+console.log(documentoIdentidade.idade);
+console.log(documentoIdentidade["empresa"]);
